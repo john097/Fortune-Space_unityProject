8a579ee2db3b53367cdcba348c142f18509219c6
Disonn <53444250+Disonn@users.noreply.github.com>
2020-04-13 13:23:34 +0800

    杰哥的AimIcon的配置

---
+++ Assets/Fx/AimIcon/AimIconCtrl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimIconCtrl : MonoBehaviour
+{
+    private float cursorPosX;
+    private float cursorPosY;
+    private float screenMaxX;
+    private float screenMaxY;
+    private float norX;
+    private float norY;
+    public float NmultX=-6.68f;
+    public float NmultY=-4.0f;
+    public float multX=6.9f;
+    public float multY=3.75f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        screenMaxX = Screen.width;
+        screenMaxY = Screen.height;
+        cursorPosX = Input.mousePosition.x;
+        cursorPosY = Input.mousePosition.y;
+        norX = cursorPosX / screenMaxX;
+        norY = cursorPosY / screenMaxY;
+        transform.localPosition = new Vector3(Mathf.Lerp(NmultX, multX, norX), Mathf.Lerp(NmultY, multY, norY), 5);
+
+    }
+}
