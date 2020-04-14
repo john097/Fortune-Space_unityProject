008e22400326fa7436d479345799811661a4299b
tonyzhenyu <zfish97@outlook.com>
2020-04-14 15:46:07 +0800

    屑UI
    屑UI

---
+++ Assets/Fx/AimIcon/AimIconFollowMouse.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AimIconFollowMouse : MonoBehaviour
+{
+
+    public Camera camera1;
+    public Vector3 v3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera1 = Camera.main;
+    }
+    void Update()
+    {
+        v3.x = camera1.ScreenToWorldPoint(Input.mousePosition).x;
+        v3.y = camera1.ScreenToWorldPoint(Input.mousePosition).y;
+        transform.position = v3;
+        
+
+    }
+}
