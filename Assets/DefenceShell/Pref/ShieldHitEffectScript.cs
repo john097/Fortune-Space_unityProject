afb15b7c9d15a597255748a4835193c759e993b5
NatsuyaXD <57437578+NatsuyaXD@users.noreply.github.com>
2020-04-19 22:40:43 +0800

    更新一下Actor以及其他资源组件

---
+++ Assets/DefenceShell/Pref/ShieldHitEffectScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldHitEffectScript : MonoBehaviour
+{
+    public GameObject bulletParent;
+    private Renderer[] thisRenderers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeShieldColor(Color c)
+    {
+        thisRenderers = bulletParent.GetComponentsInChildren<Renderer>();
+
+        //thisRenderers[0].material.GetColor("_Emissive");
+        foreach (var item in thisRenderers)
+        {
+            item.material.SetColor("_Emissive", Color.Lerp(thisRenderers[0].material.GetColor("_Emissive"),c,2f));
+        }
+    }
+}
---
+++ Assets/Script/PlayerArrowScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerArrowScript : MonoBehaviour
+{
+    public GameObject arrowPrefab;
+
+    [HideInInspector]
+        public GameObject[] target;
+
+    private GameObject player;
+
+    void Start()
+    {
+        if (!arrowPrefab)
+        {
+
+        }
+        target = GameObject.FindGameObjectsWithTag("TP_GATE");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartArrow()
+    {
+        foreach (var item in target)
+        {
+            
+        }
+    }
+}
