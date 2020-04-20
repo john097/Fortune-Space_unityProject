95a50e797b4d3b9e1b9a8d3edf99f60c6cbc80d6
Disonn <53444250+Disonn@users.noreply.github.com>
2020-04-21 00:45:43 +0800

    怪物出生特效

--- Assets/4-11关卡搬砖/monster_dead_effect.cs
+++ Assets/4-11关卡搬砖/monster_dead_effect.cs
@@ -7,17 +7,36 @@ public class monster_dead_effect : MonoBehaviour
     private Material A;
     private float effect_num=0;
     private float timer = 0;
+    private float timer1 = 0;
+    private bool born;
     private Actor mons_actor;
+
     // Start is called before the first frame update
     void Start()
     {
+        born = false;
         A=GetComponent<Renderer>().material;
        mons_actor= gameObject.transform.root.gameObject.GetComponent<Actor>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!born)
+        {
+            timer1 += Time.deltaTime;
+            if (timer1 > 2f)
+            {
+                timer1 = 2f;
+                born = true;
+
+            }
+            effect_num = Mathf.Lerp(1, 0, timer1);
+            A.SetFloat("_Alpha_Dis", effect_num);
+        }
+
+
         if (!mons_actor.isAlive)
         {
             timer += Time.deltaTime;
