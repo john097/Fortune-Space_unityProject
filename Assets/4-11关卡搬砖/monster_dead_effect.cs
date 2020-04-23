c68aaf5e7f4a9fe1503edbba3ea02e24fda74dbf
Disonn <53444250+Disonn@users.noreply.github.com>
2020-04-23 14:10:49 +0800

    怪物光环淡入淡出效果挂载

--- Assets/4-11关卡搬砖/monster_dead_effect.cs
+++ Assets/4-11关卡搬砖/monster_dead_effect.cs
@@ -23,31 +23,62 @@ public class monster_dead_effect : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!born)
-        {
-            timer1 += Time.deltaTime;
-            if (timer1 > 2f)
-            {
-                timer1 = 2f;
-                born = true;
+       
 
+        if (gameObject.name != "Ring")
+        {
+            if (!born)
+            {
+                timer1 += Time.deltaTime;
+                if (timer1 > 2f)
+                {
+                    timer1 = 2f;
+                    born = true;
+
+                }
+                effect_num = Mathf.Lerp(1, 0, timer1);
+                A.SetFloat("_Alpha_Dis", effect_num);
             }
-            effect_num = Mathf.Lerp(1, 0, timer1);
-            A.SetFloat("_Alpha_Dis", effect_num);
+
+
+            if (!mons_actor.isAlive)
+            {
+                timer += Time.deltaTime;
+                if (timer > 4f)
+                {
+                    timer = 4f;
+                }
+                effect_num = Mathf.Lerp(0, 1, timer);
+                A.SetFloat("_Alpha_Dis", effect_num);
+            }
+        }
+        else
+        {
+            if (!born)
+            {
+                timer1 += Time.deltaTime;
+                if (timer1 > 4f)
+                {
+                    timer1 = 4f;
+                    born = true;
+
+                }
+                effect_num = Mathf.Lerp(0, 1, timer1);
+                A.SetFloat("_RingAlphaClip", effect_num);
+            }
+            if (!mons_actor.isAlive)
+            {
+                timer += Time.deltaTime;
+                if (timer > 3f)
+                {
+                    timer = 3f;
+                }
+                effect_num = Mathf.Lerp(1, 0, timer);
+                A.SetFloat("_RingAlphaClip", effect_num);
+            }
         }
 
 
-        if (!mons_actor.isAlive)
-        {
-            timer += Time.deltaTime;
-            if (timer > 4f)
-            {
-                timer = 4f;
-            }
-            effect_num = Mathf.Lerp(0, 1, timer);
-            A.SetFloat("_Alpha_Dis", effect_num);
-        }
-        
     }
 
 }
