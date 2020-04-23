37ebae4d49ba9bca770bcd2b47753f73f7aec1b4
GhostMiner <1206951991@qq.com>
2020-04-23 20:42:29 +0800

    L2视效调整，停机坪H材质，停机坪

---
+++ Assets/Moduel Scene/NewTexture/CommonMat/ShopWallDes.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopWallDes : MonoBehaviour
+{
+    public AnimationCurve ac;
+    private GameObject shopArea;
+    private bool fresh = false;
+    private float time;
+    private bool trigger;
+    private bool revert;
+    private void Start()
+    {
+        shopArea = this.gameObject;
+    }
+    private void Update()
+    {
+        if (fresh) {
+            foreach (Transform item in transform)
+            {
+                time += Time.deltaTime;
+                item.GetComponent<Renderer>().material.SetFloat("_ShopWallAlpha", ac.Evaluate(time));
+            }
+        }
+        else
+        {
+            foreach (Transform item in transform)
+            {
+                time -= Time.deltaTime;
+                item.GetComponent<Renderer>().material.SetFloat("_ShopWallAlpha", ac.Evaluate(time));
+            } 
+        }
+        time = Mathf.Clamp(time, 0, 1);
+
+    }
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="Player")
+        {
+            fresh = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            fresh = false;
+        }
+    }
+}
