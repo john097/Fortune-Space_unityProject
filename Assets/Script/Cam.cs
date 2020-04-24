8e78ec13b590e834b1229bc9e3b62c2b65a3f54c
NatsuyaXD <57437578+NatsuyaXD@users.noreply.github.com>
2020-04-24 17:31:07 +0800

    Loading界面随机武器生成

--- Assets/Script/Cam.cs
+++ Assets/Script/Cam.cs
@@ -9,12 +9,18 @@ public class Cam : MonoBehaviour
     //要跟随的物体
     public GameObject m_Player;
 
+    
+
     public float smooth;
     // Use this for initialization
     void Start()
     {
         //获取到摄像机于要跟随物体之间的距离
         Dir = m_Player.transform.position - transform.position;
+
+        
+
+        
     }
 
     // Update is called once per frame
@@ -23,4 +29,6 @@ public class Cam : MonoBehaviour
         //摄像机的位置
         transform.position = Vector3.Lerp(transform.position, m_Player.transform.position - Dir, Time.deltaTime * smooth);
     }
+
+    
 }
---
+++ Assets/Script/CamShake.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamShake : MonoBehaviour
+{
+    private CinemachineCameraOffset vCam;
+    private Vector3 currentOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (transform.GetChild(transform.childCount).GetComponent<CinemachineCameraOffset>())
+        {
+            vCam = transform.GetChild(transform.childCount).GetComponent<CinemachineCameraOffset>();
+            currentOffset = vCam.m_Offset;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CameraShake(float x, float y, float z)
+    {
+        
+    }
+}
