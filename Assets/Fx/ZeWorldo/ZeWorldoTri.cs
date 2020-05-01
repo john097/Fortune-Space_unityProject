44799ecfb7fcb216b9991897d3363102b0e4004f
GhostMiner <1206951991@qq.com>
2020-05-01 22:27:57 +0800

    探测特效

---
+++ Assets/Fx/ZeWorldo/ZeWorldoTri.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZeWorldoTri : MonoBehaviour
+{
+    public GameObject zwb;
+    public GameObject zwpe;
+    private Animation zwbAnim;
+    private Animation zwpeAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        zwbAnim = zwb.GetComponent<Animation>();
+        zwpeAnim = zwpe.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            zwbAnim.CrossFade("ZeWorldoBallToLarge", 1f);
+            zwpeAnim.CrossFade("ZeWorldoPostEffectToLarge", 1f);
+            
+        }
+        
+    }
+}
