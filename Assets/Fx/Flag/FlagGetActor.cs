a874bcea6dd5b740de2f33a035a423cd1524ba2f
GhostMiner <1206951991@qq.com>
2020-05-01 15:01:04 +0800

    L1视觉效果，布料，actor中子物体的碰撞盒，出生房间视效

---
+++ Assets/Fx/Flag/FlagGetActor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagGetActor : MonoBehaviour
+{
+    private Cloth thisCloth;
+    private GameObject actor;
+    private SphereCollider sc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        actor = GameObject.Find("ActorModel");
+        thisCloth = gameObject.GetComponent<Cloth>();
+        sc = actor.GetComponent<SphereCollider>();
+        addCollider(ref thisCloth,sc);
+
+    }
+    private void addCollider(ref Cloth c, SphereCollider sc)
+    {
+        ClothSphereColliderPair[] cscp = new ClothSphereColliderPair[c.capsuleColliders.Length + 1]; //重新声明碰撞器数组
+        for (int i = 0; i < c.sphereColliders.Length; i++)
+        {
+            cscp[i] = c.sphereColliders[i];                             //初始化碰撞器数组
+        }
+        cscp[cscp.Length - 1] = new ClothSphereColliderPair(sc);        //添加碰撞器
+        c.sphereColliders = cscp;
+    }
+}
