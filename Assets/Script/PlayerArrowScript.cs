865fda952da187623508ecb6aecff274f46e7af4
Disonn <53444250+Disonn@users.noreply.github.com>
2020-05-02 22:54:32 +0800

    探测技能

--- Assets/Script/PlayerArrowScript.cs
+++ Assets/Script/PlayerArrowScript.cs
@@ -7,20 +7,31 @@ public class PlayerArrowScript : MonoBehaviour
     public GameObject arrowPrefab;
 
     [HideInInspector]
-        public GameObject[] target;
+        public GameObject target;
 
     private GameObject player;
-    private GameObject[] playerArrows;
+    private GameObject playerArrows;
 
+    public int target_type;
+    public bool search_done;
+    public bool start_search;
+    public bool search_cooldown;
+    public bool arrow_born;
     void Start()
     {
         if (!arrowPrefab)
         {
             arrowPrefab = Resources.Load("Prefabs/Arrow") as GameObject;
         }
-         target = GameObject.FindGameObjectsWithTag("TP_GATE");
+        //target = GameObject.FindGameObjectsWithTag("TP_GATE");
+        
 
-        playerArrows = new GameObject[target.Length];
+        //playerArrows = new GameObject[target.Length];
+
+        search_done = false;
+        start_search = false;
+        search_cooldown = true;
+        arrow_born = false;
     }
 
     // Update is called once per frame
@@ -28,58 +39,123 @@ public class PlayerArrowScript : MonoBehaviour
     {
 
         
-        if (Input.GetKeyDown(KeyCode.T))
+        
+        if (search_cooldown&&Input.GetKeyDown(KeyCode.T))
         {
-            StartArrow();
+            start_search = true;
+            search_cooldown = false;
+
+
         }
 
+        if (start_search)
+        {
+            if (search_done&&!arrow_born)
+            {
+                StartArrow();
+                StartCoroutine(Search_Skill_Remove(5f));
+                arrow_born = true;
+            }
+        }
 
         
-        if (Input.GetKeyDown(KeyCode.Y))
-        {
-            DestroyArrow();
-        }
+        //if (Input.GetKeyDown(KeyCode.Y))
+        //{
+        //    DestroyArrow();
+        //}
+    }
+
+    IEnumerator Search_Skill_Remove(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        start_search = false;
+        search_done = false;
+        search_cooldown = true;
+        arrow_born = false;
+        DestroyArrow();
     }
 
     //创建角色指向传送门的箭头
     public void StartArrow()
     {
-        target = GameObject.FindGameObjectsWithTag("TP_GATE");
+        //target = GameObject.FindGameObjectsWithTag("TP_GATE");
 
         player = GameObject.FindGameObjectWithTag("Player");
 
-        if (player && playerArrows.Length != 0)
-        {
-            foreach (var item in playerArrows)
-            {
-                if (item != null)
-                {
-                    DestroyImmediate(item);
-                }
-            }
-        }
+        //if (player && playerArrows.Length != 0)
+        //{
+        //    foreach (var item in playerArrows)
+        //    {
+        //        if (item != null)
+        //        {
+        //            DestroyImmediate(item);
+        //        }
+        //    }
+        //}
 
-        playerArrows = new GameObject[target.Length];
+        //playerArrows = new GameObject[target.Length];
 
-        int i = 0;
+        //int i = 0;
 
-        foreach (var item in target)
-        {
-            playerArrows[i] = Instantiate(arrowPrefab, player.transform);
-            playerArrows[i].GetComponent<Arrow>().target = target[i];
-            i++;
-        }
+        //foreach (var item in target)
+        //{
+        //    playerArrows[i] = Instantiate(arrowPrefab, player.transform);
+        //    playerArrows[i].GetComponent<Arrow>().target = target[i];
+        //    i++;
+        //}
+
+        playerArrows = Instantiate(arrowPrefab, player.transform);
+        playerArrows.GetComponent<Arrow>().target = target;
     }
 
     //删除角色身上的指向箭头
     public void DestroyArrow()
     {
-        if (playerArrows.Length != 0)
+        //if (playerArrows.Length != 0)
+        //{
+        //    foreach (var item in playerArrows)
+        //    {
+        //        DestroyImmediate(item);
+        //    }
+        //}
+        DestroyImmediate(playerArrows);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        
+        if (!search_done)
         {
-            foreach (var item in playerArrows)
+            if (other.gameObject.layer == LayerMask.NameToLayer("Tool"))
             {
-                DestroyImmediate(item);
+                switch (other.gameObject.tag)
+                {
+                    case "TP_GATE":
+                        target = other.gameObject;
+                        Debug.Log("asd");
+                        search_done = true;
+                        break;
+                    case "Blood_Box":
+                        target = other.gameObject;
+                        search_done = true;
+                        break;
+                    case "Gold_Box":
+                        target = other.gameObject;
+                        Debug.Log("asd");
+                        search_done = true;
+                        break;
+                    case "Miracle_Box":
+                        target = other.gameObject;
+                        search_done = true;
+                        break;
+                    case "Power_Box":
+                        target = other.gameObject;
+                        search_done = true;
+                        break;
+                }
             }
         }
+
     }
+
 }
