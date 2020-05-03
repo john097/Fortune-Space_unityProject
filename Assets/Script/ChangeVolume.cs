59b5b05130d582837d7f4cb93b01b7565d909bff
NatsuyaXD <57437578+NatsuyaXD@users.noreply.github.com>
2020-05-03 23:02:15 +0800

    音量设置

---
+++ Assets/Script/ChangeVolume.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class ChangeVolume : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Slider audioSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        float f;
+        audioMixer.GetFloat("Main",out f);
+        audioSlider.value = f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetMainVolume()
+    {
+        audioMixer.SetFloat("Main",audioSlider.value);
+    }
+}
