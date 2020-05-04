ee0db04e7dee0e41a4e81584c70115439c7883a1
GhostMiner <1206951991@qq.com>
2020-05-05 01:23:29 +0800

    boss房间修改

---
+++ Assets/Moduel Scene/NewTexture/Frence1/BossChangeGround.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossChangeGround : MonoBehaviour
+{
+    public GameObject ground;
+    private Material groundMat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        groundMat = ground.GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        groundMat.SetVector("_Pos", gameObject.transform.position);
+    }
+}
